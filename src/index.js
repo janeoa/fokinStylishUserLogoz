f23dfe182e1baf15bc1d729b54b8ff2c10dce1eb
Asset Malik <asetmalik@gmail.com>
2021-03-20 05:46:33 +0600

    draw canvas and resize

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import user_logo from './user_logo.jpg'
+import Sketch from 'react-p5'
+
+function drawCanvas(){
+    // console.log(user_logo)
+    let img;
+    const setup = (p5, canvasParentRef) => {
+        p5.createCanvas(100, 100).parent(canvasParentRef)
+        img = p5.loadImage("/images/user_logo.jpg");
+    }
+    const draw = p5 => {
+        // 
+        p5.image(img, 0, 0);
+        img.resize(100,100);
+      }
+      
+      return <Sketch setup={setup} draw={draw} />
+}
+
 
 
 class StylizedImage extends React.Component{
@@ -11,9 +29,7 @@ class StylizedImage extends React.Component{
                 <h1>INPUT</h1>
                 <img src={user_logo} alt="input" width="100px" />
                 <h1>OUTPUT</h1>
-                <canvas width="100" height="100">
-                    
-                </canvas>
+                {drawCanvas()}
                 <StylizedImageOption/>
                 <StylizedImageOption/>
                 <StylizedImageOption/>
